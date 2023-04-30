79cda7f74a6d1d40b308c30fafa7b990d5f6dab4
yu.wang <joeyqaq@gmail.com>
2023-04-30 15:04:14 +0800

    Initial Commit

---
+++ Subery/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Subery
+//
+//  Created by yu wang on 2023/4/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Subery/SuberyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuberyApp.swift
+//  Subery
+//
+//  Created by yu wang on 2023/4/30.
+//
+
+import SwiftUI
+
+@main
+struct SuberyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
