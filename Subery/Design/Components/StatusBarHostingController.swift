617c5d8f08b75fee20fc2e764eec89600ff289f8
joeytat <joeyqaq@gmail.com>
2023-05-04 18:04:53 +0800

    feat: update status bar to light content

---
+++ Subery/Design/Components/StatusBarHostingController.swift
@@ -0,0 +1,25 @@
+//
+//  StatusBarHostingController.swift
+//  Subery
+//
+//  Created by yu wang on 2023/5/4.
+//
+
+import SwiftUI
+
+class StatusBarHostingController<Content>: UIHostingController<Content> where Content: View {
+  private var lightStatusBar: Bool
+
+  init(rootView: Content, lightStatusBar: Bool) {
+    self.lightStatusBar = lightStatusBar
+    super.init(rootView: rootView)
+  }
+
+  @objc required dynamic init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return lightStatusBar ? .lightContent : .default
+  }
+}
--- Subery/SuberyApp.swift
+++ Subery/SuberyApp.swift
@@ -7,20 +7,37 @@
 
 import SwiftUI
 import ComposableArchitecture
-
 @main
-struct SuberyApp: App {
+struct YourAppName: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
   var body: some Scene {
-    WindowGroup {
-      NavigationView {
-        ContentView(
-          store: Store(
-            initialState: AppState.State(),
-            reducer: AppState()
-          )
-        )
-      }
-      .navigationViewStyle(.stack)
+    WindowGroup {}
+  }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    let sceneConfiguration = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    sceneConfiguration.delegateClass = SceneDelegate.self
+    return sceneConfiguration
+  }
+}
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  var window: UIWindow?
+
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    if let windowScene = scene as? UIWindowScene {
+      let window = UIWindow(windowScene: windowScene)
+      let rootView = ContentView(store: Store(
+        initialState: AppState.State(),
+        reducer: AppState()
+      ))
+      let hostingController = StatusBarHostingController(rootView: rootView, lightStatusBar: true)
+      window.rootViewController = hostingController
+      self.window = window
+      window.makeKeyAndVisible()
     }
   }
 }
