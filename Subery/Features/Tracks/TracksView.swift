1cba57fdc60461649b612d4f7451ddd0abe1200e
joeyqaq <joeyqaq@gmail.com>
2023-06-19 22:21:28 +0800

    feat: list appearances

--- Subery/Features/Tracks/TracksView.swift
+++ Subery/Features/Tracks/TracksView.swift
@@ -12,53 +12,118 @@ struct TracksView: View {
   let store: StoreOf<TracksFeature>
   var body: some View {
     WithViewStore(self.store) { viewStore in
-      NavigationView {
-        List {
-          ForEach(viewStore.tracks) { track in
-            HStack {
-              VStack(alignment: .leading) {
-                Text(track.name)
-                Text(track.category)
-              }
-              Spacer()
-              Text(track.price)
-            }
-            .swipeActions(edge: .trailing) {
-              Button {
-                viewStore.send(.deleteButtonTapped(id: track.id))
-              } label: {
-                Image(systemName: "trash")
-                  .foregroundColor(Color.daisy.warning)
-              }
-            }
-          }
-        }
-        .navigationTitle("Subery")
-        .navigationBarItems(
-          trailing: Button(
+      VStack {
+        navBar
+        listHeader
+        list
+      }
+      .background(GradientBackgroundView())
+    }
+  }
+
+  private var navBar: some View {
+    WithViewStore(self.store) { viewStore in
+      ZStack {
+        GradientBackgroundView()
+        Text("Subery")
+          .font(.title)
+          .fontWeight(.semibold)
+          .foregroundColor(Color.white)
+
+        HStack {
+          Spacer()
+          Button(
             action: {
               viewStore.send(.addTrackButtonTapped)
             },
             label: {
-              Text("Add")
+              Image(systemName: "plus.app.fill")
+                .foregroundColor(Color.daisy.accent)
+                .font(.title)
             }
-          )
-        )
-        .sheet(
-          store: self.store.scope(state: \.$destination, action: { .destination($0) }),
-          state: /TracksFeature.Destination.State.addTrack,
-          action: TracksFeature.Destination.Action.addTrack
-        ) { addTrackStore in
-          NavigationStack {
-            AddTrackView(store: addTrackStore)
-          }
+          ).padding(.trailing)
         }
-        .alert(
-          store: self.store.scope(state: \.$destination, action: { .destination($0) }),
-          state: /TracksFeature.Destination.State.alert,
-          action: TracksFeature.Destination.Action.alert
-        )
       }
+      .frame(height: 44.0)
+    }
+  }
+
+  private var listHeader: some View {
+    WithViewStore(self.store) { viewStore in
+      VStack {
+        Divider()
+          .background(Color.daisy.neutralContent)
+
+        HStack {
+          Text("Service")
+            .font(.headline)
+            .foregroundColor(Color.white)
+
+          Spacer()
+
+          Text("Price")
+            .font(.headline)
+            .foregroundColor(Color.white)
+        }
+        .padding()
+
+        Divider()
+          .background(Color.daisy.neutralContent)
+      }
+    }
+  }
+
+  private var list: some View {
+    WithViewStore(self.store) { viewStore in
+      List {
+        ForEach(viewStore.tracks) { track in
+          VStack {
+            HStack {
+              VStack(alignment: .leading) {
+                Text(track.name)
+                  .font(.headline)
+                  .foregroundColor(Color.white)
+
+                Text(track.category)
+                  .font(.body)
+                  .foregroundColor(Color.daisy.neutralContent)
+              }
+
+              Spacer()
+
+              Text(track.price)
+                .foregroundColor(Color.daisy.neutralContent)
+            }
+
+            Divider()
+              .background(Color.daisy.neutralContent)
+          }
+          .swipeActions(edge: .trailing) {
+            Button {
+              viewStore.send(.deleteButtonTapped(id: track.id))
+            } label: {
+              Image(systemName: "trash")
+                .foregroundColor(Color.daisy.warning)
+            }
+          }
+          .listRowBackground(GradientBackgroundView())
+        }
+      }
+      .listStyle(PlainListStyle())
+      .sheet(
+        store: self.store.scope(state: \.$destination, action: { .destination($0) }),
+        state: /TracksFeature.Destination.State.addTrack,
+        action: TracksFeature.Destination.Action.addTrack
+      ) { addTrackStore in
+        NavigationStack {
+          AddTrackView(store: addTrackStore)
+        }
+      }
+      .alert(
+        store: self.store.scope(state: \.$destination, action: { .destination($0) }),
+        state: /TracksFeature.Destination.State.alert,
+        action: TracksFeature.Destination.Action.alert
+      )
     }
   }
 }
