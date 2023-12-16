7f0dc58a9e2b5bb650c3371a89108a7de6829183
joeyqaq <joeyqaq@gmail.com>
2023-12-16 13:57:41 +0800

    feat: fix present white background issue

--- Subery/SuberyApp.swift
+++ Subery/SuberyApp.swift
@@ -8,50 +8,18 @@
 import SwiftUI
 import ComposableArchitecture
 @main
-struct YourAppName: App {
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-
+struct Subery: App {
   var body: some Scene {
-    WindowGroup {}
-  }
-}
-
-class AppDelegate: UIResponder, UIApplicationDelegate {
-  func application(
-    _ application: UIApplication,
-    configurationForConnecting connectingSceneSession: UISceneSession,
-    options: UIScene.ConnectionOptions
-  ) -> UISceneConfiguration {
-    UINavigationBar.appearance().backgroundColor = UIColor(named: "neutral")
-    UINavigationBar.appearance().isTranslucent = false
-    let sceneConfiguration = UISceneConfiguration(
-      name: "Default Configuration",
-      sessionRole: connectingSceneSession.role
-    )
-    sceneConfiguration.delegateClass = SceneDelegate.self
-    return sceneConfiguration
-  }
-}
-
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-  var window: UIWindow?
-
-  func scene(
-    _ scene: UIScene,
-    willConnectTo session: UISceneSession,
-    options connectionOptions: UIScene.ConnectionOptions
-  ) {
-    if let windowScene = scene as? UIWindowScene {
-      let window = UIWindow(windowScene: windowScene)
-      let rootView = ContentView(
-        store: Store(initialState: TracksListFeature.State()) {
-          TracksListFeature()
-        }
-      )
-      let hostingController = StatusBarHostingController(rootView: rootView, lightStatusBar: true)
-      window.rootViewController = hostingController
-      self.window = window
-      window.makeKeyAndVisible()
+    WindowGroup {
+      NavigationStack {
+        TracksListView(
+          store: Store(
+            initialState: TracksListFeature.State()
+          ) {
+            TracksListFeature()
+          }
+        )
+      }
     }
   }
 }
